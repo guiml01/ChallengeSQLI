1f7585902243485f107e60109d0eb1b1ea6c3d55
guiml01 <guilherme.martins-leite@supdevinci-edu.fr>
2026-06-03 11:53:57 +0200

    Ajout application Flask corrigée contre SQL Injection

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    message = ""
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        query = "SELECT * FROM users WHERE username=? AND password=?"
+
+        print(query)
+
+        cursor.execute(query, (username, password))
+
+        result = cursor.fetchone()
+
+        conn.close()
+
+        if result:
+            message = "Connexion réussie"
+        else:
+            message = "Connexion refusée"
+
+    return render_template("login.html", message=message)
+
+app.run(debug=False)
